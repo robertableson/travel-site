0a995e97f79c8a5365faa66229210ec61a5ac34a
Robert Ableson <robertableson@gmail.com>
2017-02-16 21:04:50 -0500

    new packages: postcss, autoprefixer, cssvars, nested css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,9 @@
 var gulp = require('gulp');
 var watch = require('gulp-watch');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
+var cssvars = require('postcss-simple-vars');
+var nested = require('postcss-nested');
 
 gulp.task('default', function(){
   console.log("LOLOLOLOLOLO");
@@ -10,7 +14,9 @@ gulp.task('html', function(){
 });
 
 gulp.task('styles', function(){
-  console.log("Styles ftw");
+  return gulp.src('./app/assets/styles/styles.css') //use this file...
+    .pipe(postcss([cssvars, nested, autoprefixer])) //manipulate with the following...
+    .pipe(gulp.dest('./app/temp/styles'));          //and copy to destination
 });
 
 gulp.task('watch', function(){
