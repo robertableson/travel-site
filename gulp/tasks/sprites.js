bf52b9361f4d1926924c65becac1525f6dea4331
Robert Ableson <robertableson@gmail.com>
2017-02-25 22:29:43 -0500

    Sprite working for star icon

---
+++ gulp/tasks/sprites.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+var svgSprite = require('gulp-svg-sprite');
+var rename = require('gulp-rename');
+
+var config = {
+  mode: {
+    css: {
+      render: {
+        css: {
+          template: './gulp/templates/sprite.css'
+        }
+      }
+    }
+  }
+};
+
+
+gulp.task('createSprite', function(){
+  return gulp.src('./app/assets/images/icons/**/*.svg')
+    .pipe(svgSprite(config))
+    .pipe(gulp.dest('./app/temp/sprite/'));
+});
+
+
+gulp.task('copySpriteCSS', function(){
+  return gulp.src('./app/temp/sprite/css/*.css')
+    .pipe(rename('_sprite.css'))
+    .pipe(gulp.dest('./app/assets/styles/modules'));
+});
